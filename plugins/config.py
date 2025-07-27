98b13bb51096bd5d347331e6dd4518e87d7c9bb8
SonuYadav-25 <info.sonuyadav2025@gmail.com>
2025-07-27 20:15:01 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -31,16 +31,14 @@ class Config(object):
 
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
 
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://infosonuyadav2025:<db_password>@cluster0.wqsfzt6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002708370916"))
     LOGGER = logging
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "7009857295"))
     SESSION_NAME = "UploaderXNTBot"
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002708370916")
-
     TG_MIN_FILE_SIZE = 2194304000
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "MovieUploaderRobot")
     ADL_BOT_RQ = {}
 
     # Set False off else True
